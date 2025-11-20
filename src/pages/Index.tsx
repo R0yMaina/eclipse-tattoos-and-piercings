36fee55764aabee9f7dba77a5ba73ceb4a80a752
mactsnow <macledontsnow@gmail.com>
2025-11-20 22:54:17 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,7 +13,7 @@ const Index = () => {
             <canvas 
               width="754" 
               height="1065" 
-              style={{ display: "block", width: "754px", height: "1065px" }}
+              style={{ display: "block", width: "754px", height: "1065px", position: "relative" }}
             />
           </div>
           <div className="hero-content"></div>
