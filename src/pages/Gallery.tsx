c2d25e9e122ae23e6588caa0cbe5b0e8d66db2dd
Roy <roymaina395@gmail.com>
2026-04-05 17:14:29 +0300

    Optimize gallery image loading speed

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -134,6 +134,7 @@ const Gallery = () => {
                   <OptimizedImage
                     src={item.src}
                     alt={item.alt}
+                    priority={index < 6}
                     className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
                   />
                 </div>
